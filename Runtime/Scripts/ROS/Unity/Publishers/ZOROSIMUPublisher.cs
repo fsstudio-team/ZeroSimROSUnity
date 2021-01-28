3715e7c4aba2ad722ffd28ff88290dafffa20e00
Micah Pearlman <micah@fsstudio.com>
2021-01-27 18:26:47 -0800

    added different coordinate systems for IMU publisher.

--- Runtime/Scripts/ROS/Unity/Publishers/ZOROSIMUPublisher.cs
+++ Runtime/Scripts/ROS/Unity/Publishers/ZOROSIMUPublisher.cs
@@ -28,6 +28,27 @@ namespace ZO.ROS.Publisher {
             set => _imuSensor = value;
         }
 
+        public enum CoordinateSystemEnum {
+            RightHanded_XBackward_YLeft_ZUp,
+            RightHanded_XRight_YDown_ZForward,       // RealSense D435i
+            Unity_LeftHanded_XRight_YUp_ZForward,   // Unity Standard
+            ROS_RightHanded_XForward_YLeft_ZUp,                 // ROS Standard
+        };
+
+        public CoordinateSystemEnum _coordinateSystem = CoordinateSystemEnum.ROS_RightHanded_XForward_YLeft_ZUp;
+        /// <summary>
+        /// The coordinate system for the published IMU data.
+        /// RightHanded_XBackward_YLeft_ZUp:  ? Some IMU's want this reference frame
+        /// RightHanded_XRight_YDown_ZForward: RealSense D435i IMU prefers this reference frame.
+        /// Unity_LeftHanded_XRight_YUp_ZForward:  Unity reference frame
+        /// ROS_RightHanded_XForward_YLeft_ZUp: ROS standard reference frame.
+        /// </summary>
+        /// <value></value>
+        public CoordinateSystemEnum CoordinateSystem {
+            get { return _coordinateSystem; }
+            set { _coordinateSystem = value; }
+        }
+
 
         private ZOROSTransformPublisher _transformPublisher = null;
         public ZOROSTransformPublisher TransformPublisher {
@@ -80,18 +101,56 @@ namespace ZO.ROS.Publisher {
         private Task OnPublishImuDelegate(ZOIMU lidar, string name, Vector3 linearAccel, Vector3 angularVelocity, Quaternion orientation) {
             _imuMessage.header.Update();
             _imuMessage.header.frame_id = TransformPublisher.ChildFrameID;
-            _imuMessage.linear_acceleration.UnityVector3 = linearAccel;
-            _imuMessage.angular_velocity.UnityVector3 = angularVelocity;
-            _imuMessage.orientation.UnityQuaternion = orientation;
 
-            // if (_referenceFrame == ReferenceFrame.RightHanded_XBackward_YLeft_ZUp) {
-            //     //  (x, y, z, w) -> (-x, -z, y, -w).
-            //     publishedOrientation = new Quaternion(-transform.rotation.z, -transform.rotation.x, transform.rotation.y, -transform.rotation.w);
-            //     // if (m_zReverse)
-            //     //     rot = new Quaternion (-rot.x, rot.y, -rot.z, rot.w);     
-            //     publishedAcceleration = new Vector3(-_acceleration.z, -_acceleration.x, _acceleration.y);
-            //     publishedAngularVelocity = new Vector3(-_angularVelocity.z, -_angularVelocity.x, _angularVelocity.y);
-            // }
+            if (_coordinateSystem == CoordinateSystemEnum.ROS_RightHanded_XForward_YLeft_ZUp) {
+                _imuMessage.linear_acceleration.UnityVector3 = linearAccel;
+                _imuMessage.angular_velocity.UnityVector3 = angularVelocity;
+                _imuMessage.orientation.UnityQuaternion = orientation;
+
+            } else if (CoordinateSystem == CoordinateSystemEnum.RightHanded_XBackward_YLeft_ZUp) {
+                //  (x, y, z, w) -> (-x, -z, y, -w).
+                _imuMessage.orientation.x = -orientation.z;
+                _imuMessage.orientation.y = -orientation.x;
+                _imuMessage.orientation.z = orientation.y;
+                _imuMessage.orientation.w = -orientation.w;
+
+                _imuMessage.linear_acceleration.x = -linearAccel.z;
+                _imuMessage.linear_acceleration.y = -linearAccel.x;
+                _imuMessage.linear_acceleration.z = linearAccel.y;
+
+                _imuMessage.angular_velocity.x = -angularVelocity.z;
+                _imuMessage.angular_velocity.y = -angularVelocity.x;
+                _imuMessage.angular_velocity.z = angularVelocity.y;
+            } else if (CoordinateSystem == CoordinateSystemEnum.RightHanded_XRight_YDown_ZForward) {
+                Quaternion flippedYOrientation = Quaternion.Euler(Vector3.Scale(orientation.eulerAngles, Vector3.up * -1));
+                _imuMessage.orientation.x = flippedYOrientation.x;
+                _imuMessage.orientation.y = flippedYOrientation.y;
+                _imuMessage.orientation.z = flippedYOrientation.z;
+                _imuMessage.orientation.w = -flippedYOrientation.w;  // negate left to right handed coordinate system
+
+                _imuMessage.linear_acceleration.x = linearAccel.x;
+                _imuMessage.linear_acceleration.y = -linearAccel.y;
+                _imuMessage.linear_acceleration.z = linearAccel.z;
+
+                _imuMessage.angular_velocity.x = angularVelocity.x;
+                _imuMessage.angular_velocity.y = -angularVelocity.y;
+                _imuMessage.angular_velocity.z = angularVelocity.z;
+
+            } else if (CoordinateSystem == CoordinateSystemEnum.Unity_LeftHanded_XRight_YUp_ZForward) {
+                _imuMessage.orientation.x = orientation.x;
+                _imuMessage.orientation.y = orientation.y;
+                _imuMessage.orientation.z = orientation.z;
+                _imuMessage.orientation.w = orientation.w;
+
+                _imuMessage.linear_acceleration.x = linearAccel.x;
+                _imuMessage.linear_acceleration.y = linearAccel.y;
+                _imuMessage.linear_acceleration.z = linearAccel.z;
+
+                _imuMessage.angular_velocity.x = angularVelocity.x;
+                _imuMessage.angular_velocity.y = angularVelocity.y;
+                _imuMessage.angular_velocity.z = angularVelocity.z;
+
+            }
 
 
             ROSBridgeConnection.Publish(_imuMessage, ROSTopic, Name);
