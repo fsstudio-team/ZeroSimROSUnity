b2898ec2a5feb26da4e6a8a1607a1eabec264a53
Micah Pearlman <micah@fsstudio.com>
2021-01-27 18:26:23 -0800

    hide update rate for some publishers as they are drivent by the sensors

--- Editor/CustomEditors/ZOROSIMUPublisherEditor.cs
+++ Editor/CustomEditors/ZOROSIMUPublisherEditor.cs
@@ -7,14 +7,14 @@ using ZO.ROS.Publisher;
 
 namespace ZO.Editor {
 
-    [CustomEditor(typeof(ZO.ROS.Publisher.ZOROSStereoImagePublisher))]
-    public class ZOROSStereoImagePublisherEditor : UnityEditor.Editor {
+    [CustomEditor(typeof(ZO.ROS.Publisher.ZOROSIMUPublisher))]
+    public class ZOROSIMUPublisherEditor : UnityEditor.Editor {
 
         /// <summary>
         /// Hides unused ROSTopic.  See: https://answers.unity.com/questions/316286/how-to-remove-script-field-in-inspector.html
         /// </summary>
         /// <value></value>
-        private static readonly string[] _dontIncludeMe = new string[] { "_ROSTopic", "_name", "_updateRateHz" };
+        private static readonly string[] _dontIncludeMe = new string[] { "_updateRateHz" };
         public override void OnInspectorGUI() {
 
             DrawPropertiesExcluding(serializedObject, _dontIncludeMe);
