3ee2911c23a4cb7a90e28d80d7e1198b9fc02e41
Micah Pearlman <micah@fsstudio.com>
2021-04-20 10:59:47 -0700

    added ros <--> unity conversion extension

---
+++ Runtime/Scripts/Util/Extensions/ZOROSConversionExtensions.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace ZO.Util.Extensions {
+    public static class ZOROSConversionExtensions {
+        public static Vector3 Ros2Unity(this Vector3 v) {
+            return new Vector3(-v.y, v.z, v.x);
+        }
+
+        public static Vector3 Unity2Ros(this Vector3 v) {
+            return new Vector3(v.z, -v.x, v.y);
+        }
+
+        public static Vector3 Ros2UnityScale(this Vector3 v) {
+            return new Vector3(v.y, v.z, v.x);
+        }
+
+        public static Vector3 Unity2RosScale(this Vector3 v) {
+            return new Vector3(v.z, v.x, v.y);
+        }
+
+        public static Quaternion Ros2Unity(this Quaternion q) {
+            return new Quaternion(q.y, -q.z, -q.x, q.w);
+        }
+
+        public static Quaternion Unity2Ros(this Quaternion q) {
+            return new Quaternion(-q.z, q.x, -q.y, q.w);
+        }
+
+    }
+
+}
