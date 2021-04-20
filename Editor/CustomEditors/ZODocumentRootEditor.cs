034b92d191a03f81cce5360152c053e1a1a16718
Micah Pearlman <micah@fsstudio.com>
2021-04-20 11:00:09 -0700

    added urdf export button

--- Editor/CustomEditors/ZODocumentRootEditor.cs
+++ Editor/CustomEditors/ZODocumentRootEditor.cs
@@ -7,30 +7,53 @@ using ZO.Document;
 
 namespace ZO.Editor {
 
+
+    /// <summary>
+    /// Saving and loading ZOSimDocumentRoot.
+    /// </summary>
     [CustomEditor(typeof(ZOSimDocumentRoot))]
     public class ZOSimDocumentRootEditor : UnityEditor.Editor {
+
+        private string _URDFExportDirectory = "";
 
         public override void OnInspectorGUI() {
 
             DrawDefaultInspector();
 
-            ZOSimDocumentRoot zoSimBaseComponent = (ZOSimDocumentRoot)target;
+            ZOSimDocumentRoot documentRoot = (ZOSimDocumentRoot)target;
 
+            /* TODO: no longer supporting ZOSim files.  Remove this.
             if (GUILayout.Button("Save ZOSim")) {
-                Debug.Log("INFO: ZOSimBaseComponent Save ZOSim");
-                if (EditorUtility.DisplayDialog("Override File:", "Override File: " + zoSimBaseComponent.ZOSimDocumentFilePath, "OK", "Cancel")) {
-                    zoSimBaseComponent.SaveToZOSimFile(zoSimBaseComponent.ZOSimDocumentFilePath);
+                Debug.Log("INFO: ZOSimDocumentRoot Save ZOSim");
+                if (EditorUtility.DisplayDialog("Override File:", "Override File: " + documentRoot.ZOSimDocumentFilePath, "OK", "Cancel")) {
+                    documentRoot.SaveToZOSimFile(documentRoot.ZOSimDocumentFilePath);
                 }
 
             }
 
             if (GUILayout.Button("Load ZOSim")) {
-                Debug.Log("INFO: ZOSimBaseComponent Load ZOSim");
-                if (EditorUtility.DisplayDialog("Override Object:", "Override Object with file: " + zoSimBaseComponent.ZOSimDocumentFilePath, "OK", "Cancel")) {
+                Debug.Log("INFO: ZOSimDocumentRoot Load ZOSim");
+                if (EditorUtility.DisplayDialog("Override Object:", "Override Object with file: " + documentRoot.ZOSimDocumentFilePath, "OK", "Cancel")) {
 
-                    zoSimBaseComponent.LoadFromZOSimFile(zoSimBaseComponent.ZOSimDocumentFilePath);
+                    documentRoot.LoadFromZOSimFile(documentRoot.ZOSimDocumentFilePath);
                 }
             }
+            */
+            if (GUILayout.Button("Export URDF...")) {
+                Debug.Log("INFO: ZOSimDocumentRoot Save URDF");
+                _URDFExportDirectory = EditorUtility.OpenFolderPanel("Select URDF export directory", _URDFExportDirectory, "");
+
+                if (_URDFExportDirectory.Length == 0) {
+                    return;
+                } else if (!System.IO.Directory.Exists(_URDFExportDirectory)) {
+                    EditorUtility.DisplayDialog("URDF Export Error", "Export root folder must be defined and folder must exist.", "Ok");
+                } else {
+                    documentRoot.ExportURDF(_URDFExportDirectory);
+                }
+
+
+            }
+
 
         }
     }
