ca041c66d7add64c9d443fe2e312cf49a4fb6f51
Micah Pearlman <micah@fsstudio.com>
2021-05-13 10:09:46 -0700

    added create child and sibling occurences

--- Editor/CustomEditors/ZOSimOccurrenceEditor.cs
+++ Editor/CustomEditors/ZOSimOccurrenceEditor.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
+using ZO.Document;
+namespace ZO.Editor {
 
-public class ZOSimOccurrenceEditor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [CustomEditor(typeof(ZOSimOccurrence))]
+    public class ZOSimOccurrenceEditor : UnityEditor.Editor {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public override void OnInspectorGUI() {
+
+            DrawDefaultInspector();
+
+            ZOSimOccurrence simOccurrence = (ZOSimOccurrence)target;
+
+            if (GUILayout.Button("Create Child Occurrence")) {
+                GameObject baseOccurrence = new GameObject($"ChildOf{simOccurrence.Name}");
+                baseOccurrence.transform.SetParent(simOccurrence.transform, false);
+                ZOSimOccurrence occurrence = baseOccurrence.AddComponent<ZOSimOccurrence>();
+                GameObjectUtility.EnsureUniqueNameForSibling(baseOccurrence);
+
+
+
+                GameObject visuals = new GameObject("visuals");
+                visuals.transform.SetParent(occurrence.transform, false);
+
+                GameObject collisions = new GameObject("collisions");
+                collisions.transform.SetParent(occurrence.transform, false);
+
+            }
+
+            if (GUILayout.Button("Create Sibling Occurrence")) {
+                GameObject baseOccurrence = new GameObject($"SiblingOf{simOccurrence.Name}");
+                baseOccurrence.transform.SetParent(simOccurrence.transform.parent, false);
+                ZOSimOccurrence occurrence = baseOccurrence.AddComponent<ZOSimOccurrence>();
+                GameObjectUtility.EnsureUniqueNameForSibling(baseOccurrence);
+
+
+                GameObject visuals = new GameObject("visuals");
+                visuals.transform.SetParent(occurrence.transform, false);
+
+                GameObject collisions = new GameObject("collisions");
+                collisions.transform.SetParent(occurrence.transform, false);
+
+            }
+
+        }
     }
 }
