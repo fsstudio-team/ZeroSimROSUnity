aff4952a6f39fdaaa798fe48d375a04a7c67d768
Micah Pearlman <micah@fsstudio.com>
2021-07-02 10:38:52 -0700

    urdf import can import basic hierarchy (nothing else)

--- Runtime/Scripts/Util/ImportExport/ZOImportURDF.cs
+++ Runtime/Scripts/Util/ImportExport/ZOImportURDF.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 using System.Text;
 using ZO.Document;
 using ZO.Physics;
@@ -16,28 +17,73 @@ namespace ZO.ImportExport {
 
     public class ZOImportURDF {
 
-        public ZOSimDocumentRoot Import(string urdfFilePath) {
-            using(StreamReader streamReader = new StreamReader(urdfFilePath)) {
+        public static ZOSimDocumentRoot Import(string urdfFilePath) {
+            using (StreamReader streamReader = new StreamReader(urdfFilePath)) {
                 XmlDocument xmlDocument = new XmlDocument();
                 xmlDocument.Load(streamReader);
                 return Import(xmlDocument);
             }
         }
 
-        public ZOSimDocumentRoot Import(XmlDocument xmlDocument) {                        
+        public static ZOSimDocumentRoot Import(XmlDocument xmlDocument) {
             XmlNode robot = xmlDocument.GetChildByName("robot");
 
-            GameObject rootObject = new GameObject(robot.Name);            
+            GameObject rootObject = new GameObject(robot.Name);
 
             ZOSimDocumentRoot simDocumentRoot = rootObject.AddComponent<ZOSimDocumentRoot>();
 
+            // get the joints & links
+            XmlNode[] xmlLinks = robot.GetChildrenByName("link");
+            Dictionary<string, Tuple<XmlNode, GameObject>> goLinks = new Dictionary<string, Tuple<XmlNode, GameObject>>();
 
-            // create the URDF links in Unity 
-            XmlNode[] links = robot.GetChildrenByName("link");
-            foreach(XmlNode link in links) {
-                // process the visuals
-                XmlNode[] visuals = link.GetChildrenByName("visual");
+            // find root link. which is a link without an explicit parent
 
+            // create links
+            foreach (XmlNode xmlLink in xmlLinks) {
+
+                string linkName = xmlLink.Attributes["name"].Value;
+                GameObject goLink = new GameObject(linkName);
+                ZOSimOccurrence occurrence = goLink.AddComponent<ZOSimOccurrence>();
+
+                GameObject goVisualsEmpty = new GameObject("visuals");
+                goVisualsEmpty.transform.SetParent(goLink.transform);
+                
+                GameObject goCollisionsEmpty = new GameObject("collisions");
+                goCollisionsEmpty.transform.SetParent(goLink.transform);
+
+                goLinks[linkName] = new Tuple<XmlNode, GameObject>(xmlLink, goLink);
+                // // process the visuals
+                // XmlNode[] visuals = xmlLink.GetChildrenByName("visual");
+
+                // foreach (XmlNode visual in visuals) {
+
+                // }
+
+                // get the origin
+                // XmlNode origin = 
+
+            }
+
+            // create hierarchy from joints
+            XmlNode[] xmlJoints = robot.GetChildrenByName("joint");
+            foreach (var goLink in goLinks) {
+
+                // find link parent
+                GameObject linkParent = rootObject;
+                foreach (XmlNode joint in xmlJoints) {
+                    XmlNode xmlChildLink = joint.GetChildByName("child");
+                    string childName = xmlChildLink.Attributes["link"].Value;
+
+                    if (goLink.Value.Item2.name == childName) {
+                        XmlNode xmlParentLink = joint.GetChildByName("parent");
+                        string parentName = xmlParentLink.Attributes["link"].Value;
+                        linkParent = goLinks[parentName].Item2;
+                        break;
+                    }
+
+                }
+
+                goLink.Value.Item2.transform.SetParent(linkParent.transform);
             }
 
             return simDocumentRoot;
