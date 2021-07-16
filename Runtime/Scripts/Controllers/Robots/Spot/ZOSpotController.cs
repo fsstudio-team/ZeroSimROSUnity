65b6928d8b77da53ef03db2fe32828220c8422a6
Micah Pearlman <micah@fsstudio.com>
2021-07-16 11:15:56 -0700

    fixed spot controller compile error

---
+++ Runtime/Scripts/Controllers/Robots/Spot/ZOSpotController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZO.Util;
+using ZO.Physics;
+using ZO.Sensors;
+
+namespace ZO.Controllers {
+
+    public class ZOSpotController : ZOGameObjectBase {
+
+        protected override void ZOStart() {
+            base.ZOStart();
+
+
+        }
+
+
+        protected override void ZOUpdate() {
+            base.ZOUpdate();
+
+
+        }
+
+        protected override void ZOFixedUpdate() {
+            base.ZOFixedUpdate();
+
+
+        }
+
+
+
+        protected override void ZOOnGUI() {
+
+        }
+    }
+}
